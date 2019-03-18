578332f25d0196a5ce0c74defd54de8a3d60a443
yoyo <youyou.xu@enesource.com>
2019-03-18 15:20:57 +0800

    修改了根目录下开始关闭文件的目录

--- requestnew/start.sh
+++ requestnew/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup python3 /home/pj/requestnew/manage.py runserver 0.0.0.0:8000 >> ../logs/request.log &
+nohup python3 ./requestnew/manage.py runserver 0.0.0.0:8000 >> ../logs/request.log &
 echo "启动成功"
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup python3 /home/pj/requestnew/manage.py runserver 0.0.0.0:8001 >> ./request.log &
+nohup python3 ./manage.py runserver 0.0.0.0:8001 >> ./request.log &
 echo "启动成功"
